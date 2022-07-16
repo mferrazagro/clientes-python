e3a7cc326b4cb3ba5c047047780c80bd34f00ac8
Mauricio Ferraz <104685537+mferrazagro@users.noreply.github.com>
2022-07-16 13:09:47 -0300

    alterações função

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 # View - o que vai para o usuário do sistema
-usuario_digitado = input("Informe o seu usuário: ")
-senha_digitado = input("Informe sua senha:  ")
+def view():
+    usuario_digitado = input("Informe o seu usuário: ")
+    senha_digitado = input("Informe sua senha:  ")
+    controller(usuario_digitado, senha_digitado)
 
 # Model - O que vem do banco de dados (BD)
-usuario_BD = 'joao'
-senha_BD = '123'
+def model_usuario():
+    usuario_BD = 'joao'
+    return usuario_BD
+
+def model_senha():
+    senha_BD = '123'
+    return senha_BD
 
 # Controller - a validação
-if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-    print("pode entrar")
-else: 
-    print("usuário ou senha inválido")
+def controller(usuario_digitado, senha_digitado):
+    usuario_BD = model_usuario()
+    senha_BD = model_senha()
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else: 
+        print("usuário ou senha inválido")
+
+
+view()
+
