358354e59f237982af934aac4e689518e4e319dc
Mauricio Ferraz <104685537+mferrazagro@users.noreply.github.com>
2022-07-16 13:52:25 -0300

    banco

---
+++ validacao.py
@@ -0,0 +1,9 @@
+# Controller - a validação
+import banco
+def validar_login(usuario_digitado, senha_digitado):
+    usuario_BD = banco.model_usuario()
+    senha_BD = banco.model_senha()
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else: 
+        print("usuário ou senha inválido")
